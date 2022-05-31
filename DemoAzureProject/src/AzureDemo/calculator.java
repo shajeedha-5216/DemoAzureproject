0096ee568313994ce2312df758a62f814fe4c544
shajeedha-5216 <82802841+shajeedha-5216@users.noreply.github.com>
2022-05-31 20:56:46 +0530

    Files added for calculator

---
+++ DemoAzureProject/src/AzureDemo/calculator.java
@@ -0,0 +1,14 @@
+package AzureDemo;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		
+		int a=2;
+		int b=5;
+		System.out.println(a+b);
+		System.out.println(b-a);
+
+	}
+
+}
